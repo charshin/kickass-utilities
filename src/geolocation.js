aaee3ceceea889ef0ed2fc1d30bcb1e96a868ead
Zeke Tan <zeke.tan@snaphunt.com>
2020-05-22 11:19:22 +0800

    ⚡️Add fetch geolocation and countryCodeOptions

---
+++ src/geolocation.js
@@ -0,0 +1,42 @@
+import 'isomorphic-fetch';
+import { countries } from 'country-data';
+
+const GELOCOATION_DB_URL = 'https://geolocation-db.com/json';
+
+const countriesWithoutFlag = [
+  'aq',
+  'bl',
+  'bq',
+  'cw',
+  'gg',
+  'im',
+  'je',
+  'mf',
+  'ss',
+  'sx',
+];
+
+const countryCodeOptions = countries.all
+  .filter(
+    ({ status, alpha2, countryCallingCodes }) =>
+      status === 'assigned' &&
+      alpha2 &&
+      !countriesWithoutFlag.includes(alpha2.toLowerCase()) &&
+      countryCallingCodes?.[0],
+  )
+  .map(({ name, alpha2, countryCallingCodes }) => ({
+    key: alpha2,
+    value: `${name} (${countryCallingCodes?.[0]})`,
+    text: `(${alpha2}) ${countryCallingCodes?.[0]}`,
+    flag: alpha2.toLowerCase(),
+  }));
+
+const fetchGeolocation = async () => {
+  try {
+    return await (await fetch(GELOCOATION_DB_URL)).json();
+    // eslint-disable-next-line no-empty
+  } catch {}
+  return null;
+};
+
+export { countryCodeOptions, fetchGeolocation };
--- src/index.js
+++ src/index.js
@@ -202,3 +202,4 @@ export {
   removeByPath,
   initialsOf,
 };
+export * from './geolocation';
